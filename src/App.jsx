ae14d88018f5a914070631fa489382fd9dc6f413
mungpham <mungpham919@gmail.com>
2026-01-29 01:16:42 +0700

    step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import axios from "./utils/axios.customize.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const fetchDate = async () => {
+      const response = await axios.get("/v1/api/test-api");
+      console.log('>>> check data axios: ', response.data)
+    }
+    fetchDate();
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      hello world
     </>
   )
 }
